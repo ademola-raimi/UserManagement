e9d54ab28f95ef9da09bd64967d377788e9a172c
Ademola Raimi <ademolaraimi.nig@gmail.com>
2018-08-05 18:23:59 +0100

    add schema and script for running schema

---
+++ data/schema.sql
@@ -0,0 +1,8 @@
+<?php
+
+CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name varchar(100) NOT NULL, last_name varchar(100) NOT NULL, email varchar(100) NOT NULL, created_at timestamp NULL DEFAULT NULL, updated_at timestamp NULL DEFAULT NULL, UNIQUE (email));
+INSERT INTO users (first_name, last_name, email) VALUES ('Ademola', 'Raimi', 'ademola@example.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Gorbach', 'Pavel', 'pavel@example.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Stefan', 'Knittel', 'stefan@example.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('John', 'Doe', 'john@example.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Gotye', 'Mirrors', 'gotye@example.com');
