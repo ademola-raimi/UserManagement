e2bbae367113973c88f283a9bff066a8f94be244
Ademola Raimi <ademolaraimi.nig@gmail.com>
2018-08-05 18:11:17 +0100

    add zend session and implement customize email unique validation

--- module/UserManagement/src/Controller/UserManagementController.php
+++ module/UserManagement/src/Controller/UserManagementController.php
@@ -3,6 +3,7 @@
 namespace UserManagement\Controller;
 
 use Zend\Session\Container;
+use Zend\Session\SessionManager;
 use Zend\View\Model\ViewModel;
 use UserManagement\Model\UsersTable;
 use UserManagement\Form\UserForm;
@@ -15,57 +16,59 @@ class UserManagementController extends AbstractActionController
 {
 	 private $table;
 	 private $user;
+	 private $sessionContainer;
 
     /**
      * Inject UserTable and UserManagement.
      *
-     * @return \Illuminate\Http\Response
+     * @return null
      */
     public function __construct(UsersTable $table)
     {
+        $sessionManager = new SessionManager();
+        $this->sessionContainer = new Container('ContainerNamespace', $sessionManager);
         $this->table = $table;
         $this->user = new UserManagement;
     }
 
 	/**
-     * Show the list of Books.
+     * Call add action to display form.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Http\Response
      */
     public function indexAction()
     {
-    	return new ViewModel([
-            'users' => $this->table->fetchAll(),
-        ]);
+        $this->addAction();
     }
 
     /**
-     * Show the list of Books.
+     * Show the list of users.
      *
      * @return \Illuminate\Http\Response
      */
     public function listAction()
     {
-    	return $this->indexAction(); 
+        return new ViewModel([
+            'users' => $this->table->fetchAll(),
+        ]);
     }
 
     /**
-     * Show the list of Books.
+     * Display user form and Add a user to the db.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Http\Response
      */
     public function addAction()
     {
         $request = $this->getRequest();
-
+        $data = $request->getPost();
         if (! $request->isPost()) {
             return new ViewModel();
         }
 
-        // $user = new UserManagement();
         $filter = $this->user->getInputFilter();
 
-		$isValid = $filter->setData($request->getPost())
+		$isValid = $filter->setData($data)
             ->setValidationGroup(InputFilterInterface::VALIDATE_ALL)
             ->isValid();
 
@@ -73,56 +76,158 @@ class UserManagementController extends AbstractActionController
 		{
 		    return new ViewModel([
 	            'errors' => $filter->getMessages(),
+	            'oldData' => $data,
 	        ]);
 		}
 
-		session_start();
-		$_SESSION['first_name'] = $request->getPost()->first_name;
+		if ($this->emailValidateUniqueness($data['email'])) {
+			return new ViewModel([
+	            'errors' => ['email' => ['alreadyExist' => 'email already exist in the database'] ],
+	            'oldData' => $data,
+	        ]);
+		}
 
-         $this->user->exchangeArray((array) $request->getPost());
+        $this->user->exchangeArray((array) $data);
 
-        $this->table->saveuser($this->user);
+        $user = $this->table->saveuser($this->user);
+
+        if ($user) {
+            //save and Redirect to list of users
+            $this->sessionContainer->first_name = $data->first_name;
+
+            return $this->redirect()->toRoute('list');
+        }
+        // save failure message to session and Redirect to list of users
+	    $this->sessionContainer->failure = "Something went wrong";
+
+        return $this->redirect()->toRoute('list');
+    }
+
+    private function emailValidateUniqueness($email, $id=null, $data=null)
+    {
+        $user =  $this->table->fetchAll();
+
+        $found = false;
+        if (count($user) == 0) {
+            return false;
+        }
+
+        foreach ($user as $value) {
+            if ($value->email === $email ) {
+                $found = true;
+            }
+        }
+
+        if ($id !== null) {
+            $user =  $this->table->getUser($id);
+            if ($user->email === $email) {
+                unset($data['email']);
+                return false;
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * Edit a user.
+     *
+     * @return \Http\Response
+     */
+    public function editAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+
+        if (0 === $id) {
+            return $this->redirect()->toRoute('user', ['action' => 'add']);
+        }
+
+        // Retrieve the user with the specified id. Doing so raises
+        // an exception if the user is not found, which should result
+        // in redirecting to the landing page.
+        try {
+            $user = $this->table->getUser($id);
+        } catch (\Exception $e) {
+            return $this->redirect()->toRoute('user', ['action' => 'index']);
+        }
+
+        $request = $this->getRequest();
+        $data = $request->getPost();
+
+        if (! $request->isPost()) {
+            return new ViewModel([
+	            'id' => $id,
+	            'user' => $user,
+	        ]);
+        }
+
+        $filter = $this->user->getInputFilter();
+
+		$isValid = $filter->setData($data)
+            ->setValidationGroup(InputFilterInterface::VALIDATE_ALL)
+            ->isValid();
+
+		if (!$isValid)
+		{
+            return new ViewModel([
+		        'id' => $id,
+	            'errors' => $filter->getMessages(),
+	            'user' => $user,
+	            'oldData' => $data
+	        ]);
+        }
+
+	   if ($this->emailValidateUniqueness($data['email'], $id, $data)) {
+		    return new ViewModel([
+			    'id' => $id,
+			    'user' => $user,
+	            'errors' => ['email' => ['alreadyExist' => 'email already exist in the database'] ],
+	            'oldData' => $data,
+	        ]);
+        }
+
+        $this->user->exchangeArray((array) $data);
+
+        $user = $this->table->saveuser($this->user);
+        if ($user) {
+            // save and Redirect to list of users
+            $this->sessionContainer->update_first_name = $data->first_name;
+
+            return $this->redirect()->toRoute('list');
+        }
+
+        // save to failure message to session and Redirect to list of users
+	    $this->sessionContainer->failure = "Something went wrong";
 
         return $this->redirect()->toRoute('list');
     }
 
     /**
-     * Show the list of Books.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function editAction()
-    {
-    }
-
-    /**
      * Delete a user.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Http\Response
      */
     public function deleteAction()
     {
-    	$id = (int) $this->params()->fromRoute('id', 0);
+        $id = (int) $this->params()->fromRoute('id', 0);
+
         if (!$id) {
             return $this->redirect()->toRoute('list');
         }
 
         $request = $this->getRequest();
-        if ($request->isPost()) {
-            $del = $request->getPost('del', 'No');
 
-            if ($del == 'Yes') {
-                $id = (int) $request->getPost('id');
-                $this->table->deleteUser($id);
-            }
+        $delete = $this->table->deleteUser($id);
 
-            // Redirect to list of users
+        if ($delete) {
+            // save to success message to session and Redirect to list of users
+            $this->sessionContainer->success = "User succesfully deleted";
+
             return $this->redirect()->toRoute('list');
         }
+        // save to failure message to session and Redirect to list of users
+	    $this->sessionContainer->failure = "Something went wrong";
 
-        return [
-            'id'    => $id,
-            'user' => $this->table->getUser($id),
-        ];
+        return $this->redirect()->toRoute('list');
     }
 }
